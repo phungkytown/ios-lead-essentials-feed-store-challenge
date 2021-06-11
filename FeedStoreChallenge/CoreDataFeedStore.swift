6de090da0841d0828e3f0de0b052c708a2a5109a
Hai Phung <hai.phung@peoplemedia.com>
2021-06-10 21:17:00 -0500

    CoreDataFeedStore overrides previously inserted cache

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -53,6 +53,13 @@ public final class CoreDataFeedStore: FeedStore {
 		let context = self.context
 		context.perform {
 			do {
+				let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+				request.returnsObjectsAsFaults = false
+				
+				if let cache = try context.fetch(request).first {
+					context.delete(cache)
+				}
+				
 				let managedCache = ManagedCache(context: context)
 				managedCache.timestamp = timestamp
 				managedCache.feed = NSOrderedSet(array: feed.map { local in
