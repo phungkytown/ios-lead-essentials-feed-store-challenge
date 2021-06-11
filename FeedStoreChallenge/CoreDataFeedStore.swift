00f2aba3fe8d2650f5e7154a2193349929a21cc4
Hai Phung <hai.phung@peoplemedia.com>
2021-06-10 21:32:37 -0500

    CoreDataFeedStore delivers no error on empty cache

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -81,7 +81,7 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-		fatalError("Must be implemented")
+		completion(nil)
 	}
 }
 
