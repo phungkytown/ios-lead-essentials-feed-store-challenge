f357a6555bde7e4cac7551ab523a5edea90ca2b0
Hai Phung <hai.phung@peoplemedia.com>
2021-06-15 16:23:26 -0500

    Moved ManagedFeedImage to its own file

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -73,27 +73,3 @@ public final class CoreDataFeedStore: FeedStore {
 		}
 	}
 }
-
-@objc(ManagedFeedImage)
-internal class ManagedFeedImage: NSManagedObject {
-	@NSManaged var id: UUID
-	@NSManaged var imageDescription: String?
-	@NSManaged var location: String?
-	@NSManaged var url: URL
-	@NSManaged var cache: ManagedCache
-
-	internal static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
-		return NSOrderedSet(array: localFeed.map { local in
-			let managed = ManagedFeedImage(context: context)
-			managed.id = local.id
-			managed.imageDescription = local.description
-			managed.location = local.location
-			managed.url = local.url
-			return managed
-		})
-	}
-
-	internal var local: LocalFeedImage {
-		LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
-	}
-}
---
+++ FeedStoreChallenge/ManagedFeedImage.swift
@@ -0,0 +1,33 @@
+//
+//  ManagedFeedImage.swift
+//  FeedStoreChallenge
+//
+//  Created by Hai Phung on 6/15/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import CoreData
+
+@objc(ManagedFeedImage)
+internal class ManagedFeedImage: NSManagedObject {
+	@NSManaged var id: UUID
+	@NSManaged var imageDescription: String?
+	@NSManaged var location: String?
+	@NSManaged var url: URL
+	@NSManaged var cache: ManagedCache
+
+	internal static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+		return NSOrderedSet(array: localFeed.map { local in
+			let managed = ManagedFeedImage(context: context)
+			managed.id = local.id
+			managed.imageDescription = local.description
+			managed.location = local.location
+			managed.url = local.url
+			return managed
+		})
+	}
+
+	internal var local: LocalFeedImage {
+		LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+	}
+}
